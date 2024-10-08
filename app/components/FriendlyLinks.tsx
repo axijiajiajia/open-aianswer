1c9f53156a619b4f3e648af630a7dc739fd77194
axijiajiajia <gaofangwen18@gmail.com>
2024-10-08 21:13:57 +0800

    隐私政策

--- app/components/FriendlyLinks.tsx
+++ app/components/FriendlyLinks.tsx
@@ -41,8 +41,10 @@ const FriendlyLinks = () => {
                 <path d="M3 5.25H21V18C21 18.1989 20.921 18.3897 20.7803 18.5303C20.6397 18.671 20.4489 18.75 20.25 18.75H3.75C3.55109 18.75 3.36032 18.671 3.21967 18.5303C3.07902 18.3897 3 18.1989 3 18V5.25Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M21 5.25L12 13.5L3 5.25" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
               </svg>
-              <p className="font-inter ml-2 text-xs text-black">support@aianswer.pro</p>
+              <p className="font-inter ml-2 text-s text-black">support@aianswer.pro</p>
+            
             </div>
+            <a href='/privacy-policy' className="font-inter ml-2 text-s text-black">Our Privacy Policy</a>
             {/* Add other items here */}
           </div>
         </div>
